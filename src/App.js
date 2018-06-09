375f2e4effb84def509a773f75de3cf4dba47891
Eric Laursen <ericjlaursen@gmail.com>
2018-06-09 18:44:52 -0400

    Includes handleSubmit fn that submits a message on enter

--- src/App.js
+++ src/App.js
@@ -2,26 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom";
 import io from 'socket.io-client';
 
-class App extends React.Component{
+export class App extends React.Component {
 
 	//sets initial state
-	constructor (props) {
-		super(props)
+	constructor (props){
+		super(props);
 
 		this.state = {
 			//list of messages
-			messages: [
-				{
-					from: 'Jake',
-					body: 'Test Message Body'
-				}
-			]
-		}
+			messages: []
+		};
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	//hanlde submit function
+	handleSubmit(event){
+		const body = event.target.value;
+		
+		//if enter was pressed & theres content in the textbox
+		if (event.keyCode === 13 && body){
+		console.log('submitted!')
+			const message = {
+				body,
+				from: 'Me!! '
+			};
+			
+			this.setState({messages: [message, ...this.state.messages]})
+
+			event.target.value = "";
+		};
 	}
 
 	render(){
 
-		console.log('this.state ->',this.state);
 		//map over state, create list item for each item
 		const messages = this.state.messages.map((m,i) => {
 			return <li key={i}>
@@ -43,5 +57,5 @@ class App extends React.Component{
 			)
 	}
 }
-export default App;
+
 ReactDOM.render(<App />, document.getElementById("app"));
